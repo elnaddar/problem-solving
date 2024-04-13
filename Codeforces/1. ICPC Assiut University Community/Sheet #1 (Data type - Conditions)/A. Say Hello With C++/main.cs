dd22bce9a34949cb65e775634fbd8793a4b0b01e
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-04-13 13:21:07 +0200

    solved problem 'A. Say Hello With C++' in cpp and cs.

---
+++ Codeforces/1. ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/A. Say Hello With C++/main.cs
@@ -0,0 +1,15 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+using System;
+
+namespace Codeforces.Assuit.Sheet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine($"Hello, {s}");
+        }
+    }
+}
