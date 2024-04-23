25bef1de720423f86df2a039e02f48b9073b9da5
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-04-23 18:46:59 +0200

    moved cpp solution for D bc it was in C.

--- Codeforces/1. ICPC Assiut University Community/Contest #1/D. Ali Baba and Puzzles/main.cpp
+++ Codeforces/1. ICPC Assiut University Community/Contest #1/D. Ali Baba and Puzzles/main.cpp
@@ -1,10 +1,15 @@
-// https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/C
+// https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/D
 #include<iostream>
 using namespace std;
  
 int main(){
-    char c;
-    cin >> c;
-    c = c == 'z'? 'a' : c + 1;
-    cout << c << endl;
+    long long a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+
+    bool cond1 = (a + b - c == d) || (a - b + c == d);
+    bool cond2 = (a + b * c == d) || (a * b + c == d);
+    bool cond3 = (a - b * c == d) || (a * b - c == d);
+
+    cout << (cond1 || cond2 || cond3? "YES" : "NO") << endl; 
 }
